c485613bcdcaa661e68068fa0f989b5114c3306a
sumitxt <sumitbarua14@gmail.com>
2023-02-21 23:59:01 +0600

    netlifyFix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import "./assets/css/bootstrap.css"
-import "./assets/css/mdb.min.css"
+import "./assets/css/bootstrap.css"
 import "./assets/css/animate.min.css"
 import "./assets/css/style.css"
 import App from './App';
