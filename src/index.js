637719524db61cfa2bfabd5586e27183926eb65b
sumitxt <sumitbarua14@gmail.com>
2023-02-21 13:21:21 +0600

    reduxreact

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
 import "./assets/css/bootstrap.css"
 import "./assets/css/animate.min.css"
 import "./assets/css/style.css"
+import App from './App';
+import {Provider} from "react-redux";
+import store from "./redux/store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
